def1857971b53604d22ec131b936c89f70edb761
biswajitnayaak <biswajitnayaak@gmail.com>
2022-09-19 11:24:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import PeopleBlock from './components/blocks/PeopleBlock';
-import { PeoplesDataProvider,usePeoples } from './components/blocks/PeopleBlock/Providers';
+// import PeopleBlock from './components/blocks/PeopleBlock';
+// import { PeoplesDataProvider,usePeoples } from './components/blocks/PeopleBlock/Providers';
+import {AppRoutes} from './routing/AppRoutes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
 
-  const peoplesState = usePeoples();
+  // const peoplesState = usePeoples();
 
 
   return (
     <div className="App">
-       <PeoplesDataProvider state={peoplesState}>
+
+      <Router>
+        <AppRoutes />
+      </Router>
+       {/* <PeoplesDataProvider state={peoplesState}>
         < PeopleBlock />
-      </PeoplesDataProvider>
+      </PeoplesDataProvider> */}
     </div>
   );
 }
